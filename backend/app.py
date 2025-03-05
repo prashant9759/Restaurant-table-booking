6d39ac469a4aa36b626b1ea45640e7a892bf2a35
prashant9759 <prashantkumar.20255@gmail.com>
2025-03-06 00:46:44 +0530

    fixed debug mode

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
+from sqlalchemy import text
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from werkzeug.exceptions import HTTPException
+from flask_cors import CORS
 
 import os
 from dotenv import load_dotenv
@@ -24,12 +26,13 @@ from controllers.tableInstance import blp as tableBlp
 from controllers.presentation import blp as PresentationBlp
 from controllers.user_restaurant import blp as UserRestaurantBlp
 from controllers.adminDashboard import blp as AdminDashboardBlp
-
+import sys
 from services.logout import is_token_revoked
 
 load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["API_TITLE"] = "Restaurant Table Reservation API"
 app.config["API_VERSION"] = "v1"
@@ -50,7 +53,6 @@ app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 jwt = JWTManager(app)
 
 
-
 @jwt.additional_claims_loader
 def add_claims_to_jwt(identity):
     if identity == '1':  # Admin-specific logic
@@ -69,23 +71,22 @@ def expired_token_callback(jwt_header, jwt_payload):
         "message": "The token has expired.",
         "error": "token_expired"
     }), 401
-    
-    
+
+
 @jwt.invalid_token_loader
 def invalid_token_callback(error):
     return jsonify({
         "message": "Signature verification failed.",
         "error": "invalid_token"
     }), 401
-    
-    
+
+
 @jwt.unauthorized_loader
 def missing_token_callback(error):
     return jsonify({
         "description": "Request doesn't contain an access token.",
         "error": "authorization_required"
     }), 401
-
 
 
 @app.errorhandler(HTTPException)
@@ -102,6 +103,7 @@ def handle_http_exception(e):
     response.content_type = "application/json"
     print(response.data)
     return response
+
 
 api = Api(app)
 api.register_blueprint(UserBlp)
@@ -122,62 +124,61 @@ def home():
 def seed_cuisines_and_food_preferences():
     # Seeding Cuisines
     for cuisine in CuisineEnum:
-        existing_cuisine = CuisineType.query.filter_by(name=cuisine.value).first()
+        existing_cuisine = CuisineType.query.filter_by(
+            name=cuisine.value).first()
         if not existing_cuisine:
             new_cuisine = CuisineType(name=cuisine.value)
             db.session.add(new_cuisine)
-            print(f"Added Cuisine: {cuisine.value}")
-        else:
-            print(f"Already exists: {cuisine.value}")
+            # print(f"Added Cuisine: {cuisine.value}")
+        # else:
+        #     print(f"Already exists: {cuisine.value}")
 
     # Seeding Food Preferences
     for preference in FoodPreferenceEnum:
-        existing_preference = FoodPreferenceType.query.filter_by(name=preference.value).first()
+        existing_preference = FoodPreferenceType.query.filter_by(
+            name=preference.value).first()
         if not existing_preference:
             new_preference = FoodPreferenceType(name=preference.value)
             db.session.add(new_preference)
-            print(f"Added Food Preference: {preference.value}")
-        else:
-            print(f"Already exists: {preference.value}")
+            # print(f"Added Food Preference: {preference.value}")
+        # else:
+        #     print(f"Already exists: {preference.value}")
 
     # Commit changes
     try:
         db.session.commit()
-        print("✅ All cuisines and food preferences seeded successfully.")
+        # print("✅ All cuisines and food preferences seeded successfully.")
     except Exception as e:
         db.session.rollback()
         print(f"❌ Error seeding cuisines and food preferences: {e}")
 
-from sqlalchemy import text
 
-def drop_all_tables():
-    """Drops all tables in the database irrespective of foreign keys."""
-    db.session.commit()  # Ensure all pending transactions are committed
+# def drop_all_tables():
+#     """Drops all tables in the database irrespective of foreign keys."""
+#     db.session.commit()  # Ensure all pending transactions are committed
 
-    with db.engine.connect() as connection:
-        connection.execute(text("SET FOREIGN_KEY_CHECKS = 0"))  # ✅ Use text()
+#     with db.engine.connect() as connection:
+#         connection.execute(text("SET FOREIGN_KEY_CHECKS = 0"))  # ✅ Use text()
 
-    db.reflect()  # Reflect all tables from the database
-    db.drop_all()  # Drop all tables
+#     db.reflect()  # Reflect all tables from the database
+#     db.drop_all()  # Drop all tables
 
-    with db.engine.connect() as connection:
-        connection.execute(text("SET FOREIGN_KEY_CHECKS = 1"))  # ✅ Use text()
+#     with db.engine.connect() as connection:
+#         connection.execute(text("SET FOREIGN_KEY_CHECKS = 1"))  # ✅ Use text()
 
-    print("All tables dropped successfully!")
-
+#     print("All tables dropped successfully!")
 
 
 
+
+port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+
+print("Api running on port : {} ".format(port))
 
 if __name__ == '__main__':
     with app.app_context():
         scheduler.start()
         db.create_all()
         seed_cuisines_and_food_preferences()
-        port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-        app.run(host="0.0.0.0", port=5000, debug=False)
-       
 
-
-
-
+        app.run(host="0.0.0.0", port=port, debug=False)
